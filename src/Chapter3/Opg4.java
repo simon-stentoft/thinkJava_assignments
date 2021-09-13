fbbc87a09404c2e7bcb5f81ee8bed57d048301c5
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2021-09-13 19:17:39 +0200

    hi

--- src/Chapter3/Opg4.java
+++ src/Chapter3/Opg4.java
@@ -1,12 +1,29 @@
 package Chapter3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Opg4 {
     public static void main(String[] args) {
         Random random = new Random();
         int number = random.nextInt(100) + 1;
-        System.out.println(number);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Can you guess the number between 1 and 100?");
+        System.out.print(" What is your guess?: ");
+
+        int input = scanner.nextInt();
+        System.out.println("You guessed " + input + ".");
+
+        System.out.println("The correct number is " + number + ".");
+
+        int offBy = number - input;
+
+        if (input == number)
+            System.out.println("You guessed correctly!");
+        else
+            System.out.println("You were off by: " + offBy + ".");
 
     }
 }
