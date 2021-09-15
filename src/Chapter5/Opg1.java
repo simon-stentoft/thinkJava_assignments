51c842c1287803e67bfebd8a269ad4e78af38017
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2021-09-15 09:28:22 +0200

    change

--- src/Chapter5/Opg1.java
+++ src/Chapter5/Opg1.java
@@ -4,7 +4,7 @@ public class Opg1 {
     public static void main(String[] args) {
         int x = 5;
 
-        if (x < 10) {
+        if (x > 0 && x < 10) {
             System.out.println("positive single digit number.");
         }
     }
---
+++ src/Chapter6/Opg1.java
@@ -0,0 +1,7 @@
+package Chapter6;
+
+public class Opg1 {
+    public static void main(String[] args) {
+
+    }
+}
